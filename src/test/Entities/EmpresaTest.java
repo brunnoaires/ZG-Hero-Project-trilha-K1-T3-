cb4b131625a2c4d7f55ad008f8938478465a21c9
brunnoaires <brunnoaires26@hotmail.com>
2022-09-07 13:44:50 -0300

    Junited tests

---
+++ src/test/Entities/EmpresaTest.java
@@ -0,0 +1,25 @@
+package test.Entities; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Empresa Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>set 7, 2022</pre> 
+* @version 1.0 
+*/ 
+public class EmpresaTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+
+} 
---
+++ src/test/Entities/TaskTest.java
@@ -0,0 +1,135 @@
+package test.Entities; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Task Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>set 7, 2022</pre> 
+* @version 1.0 
+*/ 
+public class TaskTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getNome() 
+* 
+*/ 
+@Test
+public void testGetNome() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setNome(String nome) 
+* 
+*/ 
+@Test
+public void testSetNome() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDesc() 
+* 
+*/ 
+@Test
+public void testGetDesc() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setDesc(String desc) 
+* 
+*/ 
+@Test
+public void testSetDesc() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getData() 
+* 
+*/ 
+@Test
+public void testGetData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setData(String data) 
+* 
+*/ 
+@Test
+public void testSetData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLvlpriori() 
+* 
+*/ 
+@Test
+public void testGetLvlpriori() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLvlpriori(String lvlpriori) 
+* 
+*/ 
+@Test
+public void testSetLvlpriori() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCat() 
+* 
+*/ 
+@Test
+public void testGetCat() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setCat(String cat) 
+* 
+*/ 
+@Test
+public void testSetCat() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
